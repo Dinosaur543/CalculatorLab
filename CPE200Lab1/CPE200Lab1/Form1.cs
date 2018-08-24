8c5c93f6b5c13d9fe1f1aeee859e092c1a044b03
Dinosaur <sou.spb@gmail.com>
2018-08-24 13:27:38 +0700

    add field

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,124 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        double first_num ;
+        double second_num;
+        double Result;
+        string resulted;
+        string Operation; 
+        public Form1() //constructor
         {
             InitializeComponent();
         }
+            
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            first_num = Convert.ToDouble(lblDisplay.Text);
+            lblDisplay.Text = "0";
+            Operation =  "/";
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = lblDisplay.Text + ".";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            string resulted;
+
+            second_num = Convert.ToDouble(lblDisplay.Text);
+            if (Operation == "+")
+                {
+                Result = (first_num + second_num);
+                lblDisplay.Text = Convert.ToString(Result);
+                first_num = Result;
+            }
+            if (Operation == "-")
+            {
+                Result = (first_num - second_num);
+                lblDisplay.Text = Convert.ToString(Result);
+                first_num = Result;
+            }
+            if (Operation == "X")
+            {
+                Result = (first_num * second_num);
+                lblDisplay.Text = Convert.ToString(Result);
+                    first_num = Result;
+            }
+            if (Operation == "÷")
+            {
+                if (second_num == 0)
+                {
+                    lblDisplay.Text = "Cannot divide by zero";
+                }
+                else
+                {
+                    Result = (first_num / second_num);
+                    lblDisplay.Text = Convert.ToString(Result);
+                    first_num = Result;
+                }
+            }
+            resulted = Convert.ToString(Result);
+
+            if (resulted.Length > 8)
+            {
+                lblDisplay.Text = "ERROR";
+            }
+        }
+
+
+      
+        private void number(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length < 8)
+            {
+                if (lblDisplay.Text == "0" && lblDisplay.Text != null)
+                {
+                    lblDisplay.Text = ((Button)sender).Text;
+                }
+                else
+                {
+                    lblDisplay.Text = lblDisplay.Text + ((Button)sender).Text;
+                }
+            }
+
+        }
+
+        private void btnOperator(object sender, EventArgs e)
+        {
+            first_num = Convert.ToDouble(lblDisplay.Text);
+            lblDisplay.Text = " ";
+            Operation = ((Button)sender).Text;
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+
+            if (lblDisplay.Text==" ")
+            {
+               Result = first_num * first_num / 100;
+               lblDisplay.Text= Convert.ToString(Result);
+            } 
+            else                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+            {
+                double secondNum = Convert.ToDouble(lblDisplay.Text);
+
+                Result = first_num * secondNum / 100;
+                lblDisplay.Text = Convert.ToString(Result);
+            }
+        }
+        
     }
 }
+
+        
+
+
+    
+
